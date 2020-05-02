221aa63ec454af76291a4bece03e1c02e53ffcf7
Israel Canessa <israellcanessa@gmail.com>
2020-05-02 04:27:17 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -2,4 +2,4 @@ greeting = "Hello World"
 name = "Fernando Israel Garcia Camacho"
 puts 'My name is '+name+''
 composer = 'Choplin'
-puts 'okay! nice to meet you! '+name'' 
+puts 'okay! nice to meet you! '+name+'' 
